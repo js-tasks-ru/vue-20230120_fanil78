774b6404f45faa800506ecdb1c59b288540dfa77
FanilZarip <fanilfan1994@mail.ru>
2023-01-31 19:20:28 +0300

    vue counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,18 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  template: `<button type="button" @click='countClicks'>{{count}}</button>`,
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    countClicks() {
+      this.count++;
+    },
+  },
+}).mount('#app');

9af67b5822560c0ed86af1989d1c0f70e375c89f
FanilZarip <fanilfan1994@mail.ru>
2023-02-01 20:23:14 +0300

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,54 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from "./vendor/vue.esm-browser.js";
+
+createApp({
+  template: `
+    <div class="row">
+        <div class="col">
+        <input type="number" v-model="operand1" />
+        </div>
+
+        <div class="col" style="display: flex; flex-direction: column; font-family: emoji">
+        <label><input type="radio" name="operator" value="sum" v-model="mathOperator" /> ➕</label>
+        <label><input type="radio" name="operator" value="subtract" v-model="mathOperator"/> ➖</label>
+        <label><input type="radio" name="operator" value="multiply" v-model="mathOperator"/> X </label>
+        <label><input type="radio" name="operator" value="divide" v-model="mathOperator"/> ➗</label>
+        </div>
+
+        <div class="col">
+        <input type="number" v-model="operand2" />
+        </div>
+
+        <div class="col">=</div>
+
+        <output class="col">{{ calculetedResult }}</output>
+    </div>
+`,
+
+  data() {
+    return {
+      mathOperator: "",
+      operand1: null,
+      operand2: null,
+    };
+  },
+
+  computed: {
+    calculetedResult() {
+      switch (this.mathOperator) {
+        case "sum":
+          return this.operand1 + this.operand2;
+        case "subtract":
+          return this.operand1 - this.operand2;
+        case "multiply":
+          return this.operand1 * this.operand2;
+        case "divide":
+          return this.operand1 / this.operand2;
+
+        default:
+          break;
+      }
+    },
+  },
+}).mount("#app");
 
 // Создайте Vue приложение

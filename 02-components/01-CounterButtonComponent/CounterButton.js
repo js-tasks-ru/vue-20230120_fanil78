ee225d86e63449c5c8e74bbfaafa55416b224863
FanilZarip <fanilfan1994@mail.ru>
2023-02-02 20:52:55 +0300

    counterButton component

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,9 +1,20 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
+import { defineComponent } from "./vendor/vue.esm-browser.js";
 
 export default defineComponent({
-  name: 'CounterButton',
+  name: "CounterButton",
 
-  // Компонент должен иметь входной параметр и порождать событие
+  props: {
+    count: {
+      default: 0,
+      type: Number,
+    },
+  },
 
-  template: `<button type="button">1</button>`,
+  methods: {
+    countClick() {
+      this.$emit("update:count", this.count + 1);
+    },
+  },
+
+  template: `<button type="button" @click="countClick">{{ count }}</button>`,
 });

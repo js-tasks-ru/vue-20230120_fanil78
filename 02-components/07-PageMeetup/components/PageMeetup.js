a3a0bb60ca29dbaebab4d91c4e75be52799cf78f
FanilZarip <fanilfan1994@mail.ru>
2023-02-03 11:19:45 +0300

    meetup list

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,83 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
-import UiContainer from './UiContainer.js';
-import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { defineComponent } from "../vendor/vue.esm-browser.js";
+import UiContainer from "./UiContainer.js";
+import UiAlert from "./UiAlert.js";
+import { fetchMeetupById } from "../meetupService.js";
+
+import MeetupView from "../../06-MeetupView/components/MeetupView";
 
 export default defineComponent({
-  name: 'PageMeetup',
+  name: "PageMeetup",
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isLoading: false,
+      meetup: {},
+      error: "",
+    };
+  },
 
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  methods: {
+    toggleLoader() {
+      this.isLoading = !this.isLoading;
+    },
+
+    getMeetup(meetupId) {
+      fetchMeetupById(meetupId)
+        .then((response) => {
+          this.handleResponse(response);
+        })
+        .catch((error) => {
+          this.handleError(error);
+        });
+    },
+    handleResponse(response) {
+      this.meetup = response;
+      this.toggleLoader();
+    },
+    handleError(error) {
+      this.error = error.message;
+      this.toggleLoader();
+    },
+
+    resetError() {
+      this.error = "";
+    },
+  },
+
+  watch: {
+    meetupId: {
+      immediate: true,
+      handler(meetupId) {
+        this.resetError();
+        this.toggleLoader();
+        this.getMeetup(meetupId);
+      },
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="!isLoading && !error" :meetup="meetup" />
 
-      <UiContainer>
+      <UiContainer v-else-if="isLoading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else>
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });

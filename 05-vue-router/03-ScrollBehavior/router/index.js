5f5c4db41d0b2ae33ce55f8e9357f4dc6eaac89a
FanilZarip <fanilfan1994@mail.ru>
2023-02-16 19:47:24 +0300

    Роутер

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -1,44 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 export const router = createRouter({
-  history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
+  history: createWebHistory("/05-vue-router/03-ScrollBehavior"),
 
   routes: [
     {
-      path: '/',
-      name: 'index',
-      component: () => import('../views/PageMeetups.vue'),
+      path: "/",
+      name: "index",
+      component: () => import("../views/PageMeetups.vue"),
     },
     {
-      path: '/meetups',
-      name: 'meetups',
-      redirect: { name: 'index' },
+      path: "/meetups",
+      name: "meetups",
+      redirect: { name: "index" },
     },
     {
-      path: '/meetups/:meetupId(\\d+)',
-      name: 'meetup',
+      path: "/meetups/:meetupId(\\d+)",
+      name: "meetup",
       meta: {
         showReturnToMeetups: true,
         saveScrollPosition: true,
       },
       props: true,
-      redirect: (to) => ({ name: 'meetup.description', params: to.params }),
-      component: () => import('../views/PageMeetup.vue'),
+      redirect: (to) => ({ name: "meetup.description", params: to.params }),
+      component: () => import("../views/PageMeetup.vue"),
       children: [
         {
-          path: '',
-          alias: 'description',
-          name: 'meetup.description',
+          path: "",
+          alias: "description",
+          name: "meetup.description",
           props: true,
-          component: () => import('../views/PageMeetupDescription.vue'),
+          component: () => import("../views/PageMeetupDescription.vue"),
         },
         {
-          path: 'agenda',
-          name: 'meetup.agenda',
+          path: "agenda",
+          name: "meetup.agenda",
           props: true,
-          component: () => import('../views/PageMeetupAgenda.vue'),
+          component: () => import("../views/PageMeetupAgenda.vue"),
         },
       ],
     },
   ],
+
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    } else if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+    } else {
+      return { left: 0, top: 0 };
+    }
+  },
 });

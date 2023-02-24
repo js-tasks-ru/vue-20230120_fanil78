8da1d1dd3ab2589c186610795d74bcd5c46d136f
FanilZarip <fanilfan1994@mail.ru>
2023-02-24 11:31:17 +0300

    Компонент ссылки

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,25 @@
 <template>
-  <a class="link">Link!</a>
+  <component :is="linkType" class="link">
+    <slot></slot>
+  </component>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+  props: {
+    tag: {
+      type: String,
+      default: 'router-link',
+    }
+  },
+
+  computed: {
+    linkType() {
+      return this.tag === 'router-link' ? 'RouterLink' : 'a'
+    }
+  }
+
 };
 </script>
 

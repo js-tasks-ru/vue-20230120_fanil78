e41ff9bf9126d6039d63f5aaff6f20851197924d
FanilZarip <fanilfan1994@mail.ru>
2023-03-14 10:45:40 +0300

    image loader

--- 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
+++ 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="image-uploader">
-    <label class="image-uploader__preview image-uploader__preview-loading" style="--bg-url: url('/link.jpeg')">
-      <span class="image-uploader__text">Загрузить изображение</span>
-      <input type="file" accept="image/*" class="image-uploader__input" />
+    <label class="image-uploader__preview" :class="{ 'image-uploader__preview-loading': loading }" :style="imageUrl">
+      <span class="image-uploader__text">{{ imageText }}</span>
+      <input
+        ref="fileInput"
+        v-bind="$attrs"
+        type="file"
+        accept="image/*"
+        class="image-uploader__input"
+        @change="setImage"
+        @click="remove"
+      />
     </label>
   </div>
 </template>
@@ -10,6 +18,83 @@
 <script>
 export default {
   name: 'UiImageUploader',
+  inheritAttrs: false,
+  props: {
+    preview: {
+      type: String,
+      required: false,
+    },
+    uploader: {
+      type: Function,
+      required: false,
+    },
+  },
+
+  emits: ['select', 'upload', 'error', 'remove'],
+
+  data() {
+    return {
+      loading: false,
+      image: this.preview,
+    };
+  },
+
+  computed: {
+    previewText() {
+      return this.image ? 'Удалить изображение' : 'Загрузить изображение';
+    },
+
+    imageText() {
+      return this.loading ? 'Загрузка...' : this.previewText;
+    },
+
+    imageUrl() {
+      return {
+        '--bg-url': `url('${this.image}')`,
+      };
+    },
+  },
+
+  methods: {
+    setImage($event) {
+      const file = $event.target.files[0];
+      this.$emit('select', file);
+
+      if (this.uploader) {
+        this.loading = true;
+        this.uploader(file)
+          .then((response) => {
+            this.$emit('upload', response);
+            this.image = response.image;
+          })
+          .catch((error) => {
+            this.$emit('error', error);
+            this.resetImage();
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      } else {
+        this.image = URL.createObjectURL(file);
+      }
+    },
+
+    remove($event) {
+      if (this.image) {
+        $event.preventDefault();
+      }
+      if (!this.loading) {
+        this.$emit('remove');
+      }
+
+      this.resetImage();
+    },
+
+    resetImage() {
+      this.image = null;
+      this.$refs.fileInput.value = '';
+    },
+  },
 };
 </script>
 

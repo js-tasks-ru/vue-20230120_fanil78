a01085778712dbd0048e781562d651b0b16cacfb
FanilZarip <fanilfan1994@mail.ru>
2023-03-21 10:00:11 +0300

    date picker

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,12 @@
 <template>
-  <UiInput />
+  <UiInput :step="step" :type="type" :value="modelValue" @change="updateDate">
+    <template v-if="$slots['left-icon']" #left-icon>
+      <slot name="left-icon" />
+    </template>
+    <template v-if="$slots['right-icon']" #right-icon>
+      <slot name="right-icon" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
@@ -7,7 +14,28 @@ import UiInput from './UiInput.vue';
 
 export default {
   name: 'UiInputDate',
+  emits: ['update:modelValue'],
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+    },
+    modelValue: {
+      type: Number,
+      required: true,
+    },
+    step: {
+      type: String,
+    },
+  },
 
   components: { UiInput },
+
+  methods: {
+    updateDate($event) {
+      this.$emit('update:modelValue', $event.target.valueAsNumber);
+    },
+  },
 };
 </script>

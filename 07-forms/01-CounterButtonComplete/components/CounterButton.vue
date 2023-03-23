bcab87f3fd97fa12e47ff5e4048698526e8de82e
FanilZarip <fanilfan1994@mail.ru>
2023-03-23 10:41:27 +0300

    Счетчик

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,34 @@
 <template>
-  <button></button>
+  <button @click="handleClick">{{ currentCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  data() {
+    return {
+      currentCount: this.count,
+    };
+  },
+
+  methods: {
+    handleClick() {
+      this.currentCount++;
+      this.$emit('update:count', this.currentCount);
+    },
+  },
+
+  watch: {
+    count(newValue) {
+      this.currentCount = newValue;
+    },
+  },
 };
 </script>
